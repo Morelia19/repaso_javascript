7b67f86a5b3000d52b6e3b273bae3c293cb02188
Morelia <morelia.gonzales.v@gmail.com>
2024-11-12 09:36:01 -0500

    Variables

---
+++ 2_variables.js
@@ -0,0 +1,28 @@
+//Esto es un comentario
+//Las variables son nombres simbólicos que pueden almacenar valores
+var nombre = "Morelia"; //Se crea la variable nombre y se le asigna el valor Morelia
+var edad = 20; //Se crea la variable edad y se le asigna el valor 20.
+var edad2 = edad + 1; //Se crea la variable edad2 y se le asigna el valor 21.
+var edad3 = edad + 2; //Se crea la variable edad3 y se le asigna el valor 22.
+
+console.log(nombre);
+console.log(edad);
+console.log(edad2); 
+console.log(edad3);
+
+//Javascript soporta varios tipos de valres
+var a = 1; //número (int)
+var b = 0.01 //decimales (float, double)
+var c = "hola"; //cadena de caracteres (String)
+var d = true; //booleano verdadero (boolean)
+var e = false; //booleano falso (boolean)
+var f = null; //nulo, que signifia "sin valor"
+var g = undefined; //que significa "sin definir", parecido a null
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f);
+console.log(g);
