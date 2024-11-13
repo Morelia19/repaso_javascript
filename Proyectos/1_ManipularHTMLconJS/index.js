f88b9e449558a4a29579fd4c75330b9e2aa671db
Morelia <morelia.gonzales.v@gmail.com>
2024-11-13 12:03:21 -0500

    proyectos 1 y 2

---
+++ Proyectos/1_ManipularHTMLconJS/index.js
@@ -0,0 +1,6 @@
+//console.log("Hola mundo");
+//window.alert("Hola mundo"); //ventana emergente de alerta
+
+//utilizamos los IDs del HTML
+document.getElementById("myH1").textContent = "Hola mundo";
+document.getElementById("myP").textContent = "Me gusta el chifa" 
---
+++ Proyectos/2_Variables_Web/index.js
@@ -0,0 +1,7 @@
+let nombreCompleto = "Morelia Gonzales";
+let edad = 20;
+let isEstudiante = true;
+
+document.getElementById("p1").textContent = `Tu nombre es ${nombreCompleto}`;
+document.getElementById("p2").textContent = `Tu edad es ${edad}`;
+document.getElementById("p3").textContent = `Estudiante: ${isEstudiante}`;
