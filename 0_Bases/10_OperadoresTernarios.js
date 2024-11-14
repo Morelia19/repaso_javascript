804284dadaad8eb07b47b60f89f10f31215e9073
Morelia <morelia.gonzales.v@gmail.com>
2024-11-14 11:03:31 -0500

    Operadores Ternarios

---
+++ 0_Bases/10_OperadoresTernarios.js
@@ -0,0 +1,16 @@
+// ternary operator = un shortcut para escribir un sentencia if else
+//                             Ayuda a asignar una bariable basada en una condición
+//                             condición ? codigoSiVerdadero : codigoSiFalso;
+
+let edad = 20;
+let mensaje = edad >=18 ? "Eres mayor" : "Eres menor";
+console.log(mensaje);
+
+let hora = 9;
+let saludo =hora > 12 ? "Buenas tardes" : "Buenas noches";
+console.log(saludo);
+
+let isEstudiante = true;
+let mensaje2 = isEstudiante ? "Eres un estudiante" : "No eres un estudiante";
+console.log(mensaje2);
+
